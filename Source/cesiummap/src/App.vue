61fdc170cdec88fd31096b1736d353a2e6e2f10b
zhangjie <googlegis@hotmail.com>
2024-04-16 17:33:15 +0800

    修改框架 vite-cesium1.116

---
+++ Source/cesiummap/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style>
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ Source/cesiummap/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+
+
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+<script setup lang="ts">
+import {onMounted, ref} from 'vue'
+import * as Cesium from 'cesium'
+
+onMounted(()=>{
+  let viewer = new Cesium.Viewer("cesiumContainer")
+})
+</script>
+<style scoped>
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.read-the-docs {
+  color: #888;
+}
+</style>
